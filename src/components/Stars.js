d2f88b868049b836c229bbd7f983b1fa5974cd6e
kriss57 <kriss@DESKTOP-KTQLM77.localdomain>
2023-03-14 07:26:18 +0100

    render mechanism for stars ok

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -2,6 +2,8 @@ import React from 'react';
 import starImg from '../_assets/images/star.png'
 import emptyStarImg from '../_assets/images/empty-star.png'
 
+import './stars.scss'
+
 const Stars = ({ stars }) => {
 
     const starsArray = []
@@ -9,11 +11,11 @@ const Stars = ({ stars }) => {
     console.log(typeof (nbStars));
     console.log(nbStars);
     console.log(starsArray);
+
     for (let i = 1; i <= 5; i++) {
         const star = i <= nbStars ? starImg : emptyStarImg
-        starsArray.push(<img key={i} src={star} alt="etoile" />)
+        starsArray.push(<img className='star' key={i} src={star} alt="etoile" />)
     }
-
 
     return (
         <div className='Stars'>
